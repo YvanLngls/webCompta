69e833dd60cde484cca186e734508697e5fc866e
yvan <yvan.langlois@etu.uca.fr>
2024-11-09 18:50:34 +0100

    ajout du composant admin-general

---
+++ front/src/app/admin/admin-general/admin-general.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../dashboard.service';
+
+@Component({
+  selector: 'app-admin-general',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-general.component.html',
+  styleUrl: './admin-general.component.css'
+})
+export class AdminGeneralComponent implements OnInit{
+
+  dbInitializedLabel:string = "";
+  dbInitialized:number = 0;
+  tableSize:number = 0;
+  categoriesSize:number = 0;
+
+  constructor(private dashboardService:DashboardService) {  }
+
+  ngOnInit(): void {
+    this.dashboardService.getListTableSize().subscribe(d=>this.tableSize = d.length)
+    this.dashboardService.getDbInitializedState().subscribe(d=>{
+      this.dbInitialized = d
+      if(d>=0){
+        if(d==0) this.dbInitializedLabel = "Not initialized"
+        else if (d==1) this.dbInitializedLabel = "Initialized" 
+      } 
+      else this.dbInitializedLabel = "Error / Not initialized"
+    })
+  
+  }
+
+}
